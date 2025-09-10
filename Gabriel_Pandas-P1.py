14d57a565234d982e455071aee1e7d47f721a630
Mary Grace Clare Gabriel <marygrace.gabriel.eng@ust.edu.ph>
2025-09-10 22:39:14 +0800

    Add files via upload

---
+++ Gabriel_Pandas-P1.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # EXPERIMENT 3
+# ## PYTHON DATA ANALYSIS (PANDAS)
+
+# ### Name: Mary Grace Clare R. Gabriel
+# ### Section: 2ECE-B
+# ### Date: September 10, 2025
+
+# In[2]:
+
+
+import pandas as pd  #import the pandas library
+
+
+# In[3]:
+
+
+cars = pd.read_csv('cars.csv') #to load the file "cars.csv"
+cars #to #display the contents of the file
+
+
+# In[4]:
+
+
+cars.head()  #to display the first 5 rows of the dataframe cars
+
+
+# In[5]:
+
+
+cars.tail() #to display the last 5 rows of the dataframe cars
+
+
+# In[ ]:
+
+
+
+
